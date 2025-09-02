76bf7bf416d2605bc1b8e16b2971056d913a152e
srikanthadri <97029711+srikanthadri@users.noreply.github.com>
2025-09-02 17:21:18 +0530

    app.py

--- app.py
+++ app.py
@@ -169,11 +169,11 @@ for _, row in filtered_polygons.iterrows():
     if (color == "green" and show_suggested) or (color == "red" and show_existing):
         folium.CircleMarker(
             location=[centroid.y, centroid.x],
-            radius=4,
+            radius=6,
             color=color,
             fill=True,
             fill_color=color,
-            fill_opacity=0.7,
+            fill_opacity=0.9,
             popup=f"ID: {row['id']}, Acreage: {row['acreage']} ha"
         ).add_to(m)
 
